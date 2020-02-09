37df780dff90ca8ea6bb51356ba2c22f300e74bd
decongo <conger.devon@gmail.com>
2020-02-09 01:28:43 -0700

    trades card

---
+++ src/helpers.js
@@ -0,0 +1,16 @@
+export const ETHER_ADDRESS = '0x0000000000000000000000000000000000000000'
+
+export const DECIMALS = (10**18)
+
+// Shortcut to avoid passing around web3 connection
+
+export const ether = (wei) => {
+  if (wei) {
+    return wei / DECIMALS
+  }
+}
+
+export const tokens = ether;
+
+export const GREEN = 'success';
+export const RED = 'danger';
--- src/store/selectors.js
+++ src/store/selectors.js
@@ -1,5 +1,7 @@
 import { get } from 'lodash';
 import { createSelector } from 'reselect'
+import moment from 'moment'
+import { ETHER_ADDRESS, tokens, ether, GREEN, RED } from '../helpers.js'
 
 const account = state => get(state, 'web3.account');
 export const accountSelector = createSelector(account, a => a);
@@ -20,3 +22,77 @@ export const contractsLoadedSelector = createSelector(
   exchangeLoaded, 
   (tl, el) => tl && el
 );
+
+const filledOrdersLoaded = state => get(state, 'exchange.filledOrders.loaded', false);
+export const filledOrdersLoadedSelector = createSelector(filledOrdersLoaded, loaded => loaded)
+
+const filledOrders = state => get(state, 'exchange.filledOrders.data', []);
+export const filledOrdersSelector = createSelector(
+  filledOrders,
+  orders => {
+    // sort orders by date ascending for price comparison
+    orders = orders.sort((a, b) => a.timestamp - b.timestamp);
+
+    // decorate the orders
+    orders = decorateFilledOrders(orders);
+
+    // sort orders by date descending for display
+    orders = orders.sort((a, b) => b.timestamp - a.timestamp);
+    console.log(orders);
+    return orders;
+  }
+)
+
+const decorateFilledOrders = orders => {
+  // track previousOrder to compare history
+  let previousOrder = orders[0];
+  return (
+    orders.map(order => {
+      order = decorateOrder(order);
+      order = decorateFilledOrder(order, previousOrder);
+      previousOrder = order // update previous order once it's decorated
+      return order;
+    })
+  )
+}
+
+const decorateOrder = order => {
+  let etherAmount, tokenAmount;
+  if (order.tokenGive == ETHER_ADDRESS) {
+    etherAmount = order.amountGive;
+    tokenAmount = order.amountGet;
+  } else {
+    etherAmount = order.amountGet;
+    tokenAmount = order.amountGive;
+  }
+
+  // calculate token price to 5 decimal places
+  const precision = 100000;
+  let tokenPrice = etherAmount / tokenAmount;
+  tokenPrice = Math.round(tokenPrice * precision) / precision;
+
+  return ({
+    ...order,
+    etherAmount: ether(etherAmount),
+    tokenAmount: tokens(tokenAmount),
+    tokenPrice,
+    formattedTimestamp: moment.unix(order.timestamp).format('h:mm:ss a M/D')
+  })
+}
+
+const decorateFilledOrder = (order, previousOrder) => {
+  return ({
+    ...order,
+    tokenPriceClass: tokenPriceClass(order.tokenPrice, order.id, previousOrder)
+  })
+}
+
+const tokenPriceClass = (tokenPrice, orderId, previousOrder) => {
+  // show green price if only one order exists
+  if (previousOrder.id === orderId) return GREEN;
+
+  // show gree price if order price higher than previous order
+  // show red price if order price lower that previous order
+  if (previousOrder.tokenPrice <= tokenPrice) return GREEN; // success
+  else return RED; // danger
+}
