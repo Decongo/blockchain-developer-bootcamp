466bd9acd3444040ba88195c7763fea962d12118
decongo <conger.devon@gmail.com>
2020-02-29 23:38:38 -0700

    deployed to kovan testnet

--- truffle-config.js
+++ truffle-config.js
@@ -1,6 +1,8 @@
 require('babel-register');
 require('babel-polyfill');
 require('dotenv').config();
+const HDWalletProvider = require('truffle-hdwallet-provider-privkey');
+const privateKeys = process.env.PRIVATE_KEYS || "";
 
 module.exports = {
 
@@ -9,7 +11,20 @@ module.exports = {
       host: "127.0.0.1",
       port: "7545",
       network_id: "*" // match any network id
-    }
+    },
+    kovan: {
+      provider: function() {
+        return new HDWalletProvider(
+          // Private Key
+          privateKeys.split(','),
+          // URL to Ethereum node
+          `https://kovan.infura.io/v3/${process.env.INFURA_API_KEY}`
+        )
+      },
+      gas: 5000000,
+      gasPrice: 25000000000,
+      network_id: 42
+    },
   },
 
   contracts_directory: './src/contracts',
